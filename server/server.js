3063d0b78e345526f4417afdff7f5bc064d7a3ce
Ian Clark <ianclark226@gmail.com>
2020-02-07 13:23:21 -0500

    2/7

--- server/server.js
+++ server/server.js
@@ -3,17 +3,27 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
+app.use(express.static('public'));
+
 app.get('/', (_, res) => {
-    const filePath = path.resolve(__dirname, '..', 'index.html');
+    const filePath = path.resolve(__dirname, '..', 'public','index.html');
 
     res.sendFile(filePath);
 });
 
 app.get('/notes', (_, res) => {
-    const filePath = path.resolve(__dirname, '..', 'notes.html');
+    const filePath = path.resolve(__dirname, '..', 'public', 'notes.html');
 
     res.sendFile(filePath);
 });
+
+app.get('/index.js', (_, res) => {
+    const filePath = path.resolve(__dirname, '..', 'public', 'notes.html');
+
+    res.sendFile(filePath);
+});
+
+
 
 app.use('*', (_, res) => {
     res.redirect('/');
