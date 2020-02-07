2a50930f2cb934a4bf64cc1ed93c40f64cf136e0
Ian Clark <ianclark226@gmail.com>
2020-02-07 12:15:47 -0500

    2/7

--- server/server.js
+++ server/server.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (_, res) => {
+    const filePath = path.resolve(__dirname, '..', 'index.html');
+
+    res.sendFile(filePath);
+});
+
+app.get('/notes', (_, res) => {
+    const filePath = path.resolve(__dirname, '..', 'notes.html');
+
+    res.sendFile(filePath);
+});
+
+app.use('*', (_, res) => {
+    res.redirect('/');
+});
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
